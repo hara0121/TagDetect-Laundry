6726ab5c8194168ab08ae6298b218fd7716c2d94
hara0121 <ryo0121_1998@icloud.com>
2023-09-19 16:40:42 +0900

    first commit

--- test.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-# TensorFlowのバージョン情報を取得
-tf_version = tf.__version__
-
-print("TensorFlowのバージョン:", tf_version)
--- test2.py
+++
@@ -1,9 +0,0 @@
-import cv2#画像や動画を処理するオープンライブラリ
-
-# 画像を読み込みます
-img = cv2.imread("D:/wash-material/40_resize/wash40.jpg")
-
-# 画像を表示するウィンドウの名前は"sample"
-# 読み込んだ画像は"img"です
-cv2.imshow("sample", img)
-cv2.waitKey(0)
